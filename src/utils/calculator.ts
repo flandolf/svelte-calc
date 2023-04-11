40dbed8bb4765559a63ff71807d9dac79e8dabad
andy <66240551+flandolf@users.noreply.github.com>
2023-04-11 18:43:03 +1000

    Formatting

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -2,7 +2,6 @@ export function evaluate(expression: string): string {
   try {
     const sanitizedExpression = expression.replace(/[^0-9+\-*/.]/g, "");
 
-
     const result = eval(sanitizedExpression);
     return result.toString();
   } catch (error) {
